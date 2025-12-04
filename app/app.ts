cb1c9fcb022e4a0eb78ad87bb61f7c733481ede3
milkeclair <milkeclair.black@gmail.com>
2025-12-05 07:44:34 +0900

    refactor: [green] 関数化

--- app/app.ts
+++ app/app.ts
@@ -1,30 +1,34 @@
 import { Backend } from '../framework/backend/backend';
 
-const appHome = new URL('./src/', import.meta.url).pathname;
-const backend = Backend({ appHome });
+function App() {
+  const appHome = new URL('./src/', import.meta.url).pathname;
+  const c = Backend({ appHome });
 
-backend.get('/', (_req, res) => {
-  res.html('<a href="/home">Go to Home</a>');
-});
+  c.get('/', (_req, res) => {
+    res.html('<a href="/home">Go to Home</a>');
+  });
 
-backend.get('/api/hello', (_req, res) => {
-  res.json({ message: 'Hello, World!' });
-});
+  c.get('/api/hello', (_req, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
 
-backend.get('/home', (_req, res) => {
-  res.render('compare_code', { message: 'Welcome!' });
-});
+  c.get('/home', (_req, res) => {
+    res.render('compare_code', { message: 'Welcome!' });
+  });
 
-backend.get('/users/:id', (req, res) => {
-  res.render('compare_code', { message: `User ID: ${req.params.id}` });
-});
+  c.get('/users/:id', (req, res) => {
+    res.render('compare_code', { message: `User ID: ${req.params.id}` });
+  });
 
-backend.get('/search', (req, res) => {
-  res.json({ query: req.query });
-});
+  c.get('/search', (req, res) => {
+    res.json({ query: req.query });
+  });
 
-backend.post('/api/data', (_req, res) => {
-  res.status(201).json({ created: true });
-});
+  c.post('/api/data', (_req, res) => {
+    res.status(201).json({ created: true });
+  });
 
-backend.start();
+  return c;
+}
+
+App().start();
